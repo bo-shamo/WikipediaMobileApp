27d1fd39586c80e731404f6a5a17a133373f538b
Dan Roesch <danielar2143@gmail.com>
2021-01-10 16:05:26 -0600

    Creating Activities Demo

---
+++ app/src/main/java/danielroesch/activities/ArticleDetailActivity.kt
@@ -0,0 +1,13 @@
+package danielroesch.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import danielroesch.org.R
+
+class ArticleDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
---
+++ app/src/main/java/danielroesch/activities/SearchActivity.kt
@@ -0,0 +1,12 @@
+package danielroesch.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import danielroesch.org.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
